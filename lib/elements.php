cd8567ac7d2aacf7b5bc8fc9098918a46e7d40e8
oelgarto <oelgarto@N20-22.SCHULE>
2014-04-02 10:04:33 +0100

    Link element added (for brian :)

---
+++ lib/elements.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'lib/element.php';
+require_once 'lib/url.php';
+
+class Link extends Element {
+	
+	private $url;
+	private $content;
+	
+	public function toHTML() {
+		
+	}
+	
+}
