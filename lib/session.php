2ece6f5be44de0e8a042c7aeca3597e6e5efcd50
Tobias Oelgarte <tbn@bryllfrosch>
2014-04-02 21:56:22 +0200

    Session:: was missing

--- lib/session.php
+++ lib/session.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * Description of session
- *
- * @author bussebr
- */
 class Session {
 
 	private static function set($key, $value) {
@@ -20,34 +15,35 @@ class Session {
 	}
 	
 	public static function setLoggedIn($loggedIn) {
-		set('logged_in', $loggedIn);
+		Session::set('logged_in', $loggedIn);
 	}
 
 	public static function isLoggedIn() {
-		return get('logged_in');
+		return Session::get('logged_in');
 	}
 	
 	public static function setLoginFailed($loginFailed) {
-		set('login_failed', $loginFailed);
+		Session::set('login_failed', $loginFailed);
 	}
 
 	public static function loginFailed() {
-		return get('login_failed');
+		return Session::get('login_failed');
 	}
 	
 	public static function setUserID($userID){
-		set('user_id', $userID);
+		Session::set('user_id', $userID);
 	}
 	
 	public static function getUserID(){
-		return get('user_id');
+		return Session::get('user_id');
 	}
 	
 	public static function setUserName($userName){
-		set('user_name', $userName);
+		Session::set('user_name', $userName);
 	}
 	
 	public static function getUserName(){
-		return get('user_name');
+		return Session::get('user_name');
 	}
+	
 }
