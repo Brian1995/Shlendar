0ad0492857d6bd73238a447961f536f1fafbbd6c
oelgarto <oelgarto@N20-22.SCHULE>
2014-04-23 10:13:29 +0100

    db

--- lib/db.php
+++ lib/db.php
@@ -2,6 +2,59 @@
 
 include_once 'lib/tools/url.php';
 
+class DatabaseConnection {
+	
+	private $server;
+	private $schema;
+	private $user;
+	private $password;
+	private $link;
+	
+	public function __construct(
+			$server   = 'localhost',
+			$schema   = 'projekt',
+			$user     = 'projekt',
+			$password = 'projekt') {
+		$this->server   = $server;
+		$this->schema   = $schema;
+		$this->user     = $user;
+		$this->password = $password;
+		$this->link     = NULL;
+	}
+	
+	public function connect() {
+		$link = mysql_connect($this->server, $this->user, $this->password, false);
+		return ($link && mysql_select_db($schema, $link));
+	}
+	
+	public function query($query) {
+		$a = array();
+		$ac = func_num_args();
+		$args = func_get_args();
+		for ($i = 1; $i < $ac; $i++) {
+			$a[] = mysql_real_escape_string($args[$i]);
+		}
+		$finalQuery = vsprintf($query, $a);
+		return mysql_query($finalQuery, $this->link);
+	}
+	
+	public static function countRows($result) {
+		return mysql_num_rows($result);
+	}
+	
+	public static function fetchRow($result) {
+		return mysql_fetch_array($result);
+	}
+	
+	public static function fetchAllRows($result) {
+		$a = array();
+		$rowCount = DatabaseConnection::countRows($result);
+		for ($i = 0; $i < $rowCount; $i++) {
+			$a[$i] = DatabaseConnection::fetchRow($result);
+		}
+	}
+}
+
 function db_get_link() {
 	$dbServer   = 'localhost';
 	$dbSchema   = 'projekt';	
--- lib/session.php
+++ lib/session.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'lib/db.php';
+
 class Session {
 
 	private static function set($key, $value) {
@@ -46,4 +48,47 @@ class Session {
 		return Session::get('user_name');
 	}
 	
+	public static function login(DatabaseConnection $dbConnection, $username, $password) {
+		
+		
+	}
+	
+//	if (filter_input(INPUT_SERVER, 'REQUEST_METHOD', FILTER_DEFAULT) === 'POST') {
+//		if (filter_has_var(INPUT_POST, 'username') && filter_has_var(INPUT_POST, 'password')) {
+//			$username = filter_input(INPUT_POST, 'username');
+//			$password = filter_input(INPUT_POST, 'password');
+//			
+//			$link = db_get_link();
+//			
+//			$query = 
+//				sprintf("SELECT id, username, password FROM users WHERE username = '%s' AND password = '%s';" ,
+//				mysql_real_escape_string($username),
+//				mysql_real_escape_string($password));
+//			$result = mysql_query($query, $link);
+//			
+//			if (!$result) {
+//				die('Fehler bei SQL Abfrage: '.mysql_error());
+//			}
+//			
+//			if (mysql_num_rows($result) == 1) {
+//			
+//				$row = mysql_fetch_array($result);
+//			
+//				// Benutzer als angemeldet speichern
+//				Session::setLoggedIn(true);
+//				Session::setUserName($username);
+//				Session::setUserID($row['id']);
+//				Session::setLoginFailed(false);
+//
+//				// redirect to start page
+//				url_redirect(url_set_query_parameter(url_full(), 'action', NULL));
+//			} else {
+//				Session::setLoginFailed(true);
+//				
+//				// redirect to login page
+//				url_redirect(url_set_query_parameter(url_full(), 'action', 'login'));
+//			}
+//		}
+//	}
+//}
 }
