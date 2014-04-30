cef2fa09f9dc23b15befbcdfb620f3e8efc17b50
bussebr <bussebr@N20-23.SCHULE>
2014-04-30 09:50:20 +0100

    session klasse angwendet

--- lib/pageelements/loginelement.php
+++ lib/pageelements/loginelement.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once 'lib/pageelements.php';
 require_once 'lib/xml.php';
 
 /**
@@ -10,12 +9,35 @@ require_once 'lib/xml.php';
  */
 class LoginElement extends PageElement {
 	
-	public function __construct() {
-		
+	private $submitUrl;
+	
+	public function __construct($submitUrl) {
+		$this->submitUrl = $submitUrl;
 	}
 	
 	public function toXML() {
 		$form = new XMLElement('form');
+		$form->addAttribute('action', $this->submitUrl);
+		$form->addAttribute('method', 'post');
+		$user = new XMLElement('input');
+		$user->addAttribute('type', 'text');
+		$user->addAttribute('name', 'username');
+		$password = new XMLElement('input');
+		$password->addAttribute('type', 'password');
+		$password->addAttribute('name', 'password');
+		$submit = new XMLElement('input');
+		$submit->addAttribute('type', 'submit');
+		$userLabel = new XMLElement('div');
+		$userLabel->addChild(new XMLText('Benutzer'));
+		$passwordLabel = new XMLElement('div');
+		$passwordLabel->addChild(new XMLText("Passwort"));
+		
+		
+		$form->addChild($userLabel);
+		$form->addChild($user);
+		$form->addChild($passwordLabel);
+		$form->addChild($password);
+		$form->addChild($submit);
 		return $form;
 	}
 }
