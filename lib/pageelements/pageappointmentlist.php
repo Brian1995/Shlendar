a9d939c77eec0de9d35c07167e87b9bed9861e73
bussebr <bussebr@N20-23.SCHULE>
2014-05-21 08:55:55 +0100

    delete appointments implemented

--- lib/pageelements/pageappointmentlist.php
+++ lib/pageelements/pageappointmentlist.php
@@ -14,21 +14,25 @@ class PageAppointmentList extends PageContainer {
 	private $calendar_id;
 
 	function __construct($dbConnection, $calendar_id) {
-		parent::__construct();
+		parent::__construct('div');
+		$this->setProperty('id', 'appointment-list');
+		
 		$this->dbConnection = $dbConnection;
 		$this->calendar_id = $calendar_id;
 	}
-
+	
+	
+	
 	public function toXML() {
 		$result = $this->dbConnection->query("SELECT * FROM appointments a WHERE a.calendar_id = '%s' ORDER BY a.start_date;", $this->calendar_id);
 
-		$list = new XMLElement('div');
+		$element = parent::toXML();
 		while ($a = mysql_fetch_row($result)) {
 			$item = new PageAppointmentListItem($a[0], $a[1], $a[2], $a[3], $a[4], $a[5]);
-			$list->addChild($item->toXML());
+			$element->addChild($item->toXML());
 		}
 
-		return $list;
+		return $element;
 	}
 
 }
